57f9a39f7732f90485b2226b23fe9210a426c087
Raphael <raphael.njoroge02@gmail.com>
2025-07-23 17:53:58 +0300

    First commit

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,9 @@
 // Your code here
+export function pointsForWord(word) {
+  let points = 0;
+  for (const char of word) {
+    // The 'i' flag makes the regex case-insensitive
+    points += /[aeiou]/i.test(char) ? 1 : 2;
+  }
+  return points;
+}
